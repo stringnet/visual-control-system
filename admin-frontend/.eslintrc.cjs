53539935d4dcfad73303535947cab9a3e7b46073
stringnet <135693628+stringnet@users.noreply.github.com>
2025-05-15 00:30:59 -0500

    Create .eslintrc.cjs

---
+++ admin-frontend/.eslintrc.cjs
@@ -0,0 +1,24 @@
+// admin-frontend/.eslintrc.cjs
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime', // Necesario para el nuevo JSX transform
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } }, // Especifica la versión de React
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/prop-types': 'off', // Puedes desactivarlo si usas TypeScript o prefieres no usar prop-types
+    // Puedes añadir o modificar reglas aquí
+    // 'no-unused-vars': 'warn', 
+  },
+}
